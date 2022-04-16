632ca361cfc47067a3d00167b157ff75c7b69751
陈小满 <pity4yeats@outlook.com>
2022-04-16 22:15:28 +0800

    fuck all templates

---
+++ fuck_templates.py
@@ -0,0 +1,30 @@
+# Helper script to avoid runtime-restart in colab run.
+# Get Eval F1 of All Templates on Full Data Scenario for Both CoNLL2003 and CoNLL2004.
+
+# Accept two arguments:
+#   @dataset: string - conll04
+#   @template: string - template06
+
+# Usage:
+#   python fuck_templates.py conll03 template02
+
+import os
+os.chdir('/content/drive/MyDrive/fuck-templates/code_dir')
+from prompt_model import *
+import sys
+
+epochs = 5
+batch_size = 32
+# dataset = 'conll04'   # modify this to load another dataset
+# template_path = './data/processed/template06'   # modify this to use different templates
+
+dataset = sys.argv[1]
+template_path = os.path.join('./data/processed/', sys.argv[2])
+
+output_dir = os.path.join('./saved_models', dataset)
+best_model_dir = os.path.join('./saved_models', dataset, 'best_model')
+train_dataset = os.path.join(template_path, '{}_train'.format(dataset))
+devel_dataset = os.path.join(template_path, '{}_devel'.format(dataset))
+
+print('\n\n\n\n\n--------------------------------\nTraining: {}/{}\n'.format(template_path, dataset))
+train(batch_size, epochs, output_dir, best_model_dir, train_dataset, devel_dataset)
