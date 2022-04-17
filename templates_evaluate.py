05591b705c1aa4cce369abc755b6ae41ac54a506
陈小满 <pity4yeats@outlook.com>
2022-04-18 01:58:44 +0800

    add train and eval scripts

---
+++ templates_evaluate.py
@@ -0,0 +1,18 @@
+import transformers
+from prompt_model import *
+
+dataset = sys.argv[1]
+template = sys.argv[2]
+
+portions = [20, 40, 60, 80, 100]
+
+if dataset == 'conll03':
+    test_dataset = os.path.join('./data/original/CoNLL03/test.txt')
+elif dataset == 'conll04':
+    test_dataset = os.path.join('./data/original/CoNLL04/test.txt')
+
+for portion in portions:
+    model_path = os.path.join('./saved_models', dataset, template, portion)
+    model = transformers.BartForConditionalGeneration.from_pretrained(model_path)
+    print('\n\n\n\n\n--------------------------------\nEvaluating: {}/{}\n'.format(template, dataset))
+    evaluate(model, test_dataset)
---
+++ templates_train.py
@@ -0,0 +1,39 @@
+# Helper script to avoid runtime-restart in colab run.
+# Get Eval F1 of All Templates on Full Data Scenario for Both CoNLL2003 and CoNLL2004.
+
+# Accept two arguments:
+#   @dataset: string - conll04
+#   @template: string - template06
+
+# Usage:
+#   python templates_train.py conll03 template02
+
+"""
+I have four machines, each running one method. Each one holding 10 training and 10 inference.
+That'll do for the other template's results.
+"""
+# --->Training<---
+# @dataset_name: conll03_20.txt, conll04_80.txt, conll04_100.txt
+# @template: template03, template05
+
+# --->Inference<---
+
+from prompt_model import *
+import sys
+
+dataset = sys.argv[1]
+template = sys.argv[2]
+
+epochs = 5
+batch_size = 32
+portions = [20, 40, 60, 80, 100]
+template_path = os.path.join('./data/processed/', template)
+
+for portion in portions:
+    output_dir = os.path.join('./saved_models', dataset, template, portion)
+    best_model_dir = os.path.join(output_dir, 'best_model')
+    train_dataset = os.path.join(template_path, '{}_train_{}.txt'.format(dataset, portion))
+    devel_dataset = os.path.join(template_path, '{}_devel_{}.txt'.format(dataset, portion))
+
+    print('\n\n\n\n\n--------------------------------\nTraining: {}/{}\n'.format(template_path, dataset))
+    train(batch_size, epochs, output_dir, best_model_dir, train_dataset, devel_dataset)
