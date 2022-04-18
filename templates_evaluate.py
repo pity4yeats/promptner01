6c5e6113196d772996343ce1ae07723f7b509199
陈小满 <pity4yeats@outlook.com>
2022-04-19 04:08:58 +0800

    use portion test data in eval script

--- templates_evaluate.py
+++ templates_evaluate.py
@@ -6,13 +6,14 @@ template = sys.argv[2]
 
 portions = [20, 40, 60, 80, 100]
 
-if dataset == 'conll03':
-    test_dataset = os.path.join('./data/original/CoNLL03/test.txt')
-elif dataset == 'conll04':
-    test_dataset = os.path.join('./data/original/CoNLL04/test.txt')
-
 for portion in portions:
     portion = str(portion)
+
+    if dataset == 'conll03':
+        test_dataset = os.path.join('./data/original/CoNLL03/test_{}.txt'.format(portion))
+    elif dataset == 'conll04':
+        test_dataset = os.path.join('./data/original/CoNLL04/test_{}.txt'.format(portion))
+
     model_path = os.path.join('./saved_models', dataset, template, portion)
     model = transformers.BartForConditionalGeneration.from_pretrained(model_path)
     print('\n\n\n\n\n--------------------------------\nEvaluating: {}/{}/{}\n'.format(dataset, template, portion))
